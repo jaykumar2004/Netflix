a03ee2bca8a858caf6241d1c76e646299089be31
Jay Kumar Jangid <106320129+jaykumar2004@users.noreply.github.com>
2025-05-01 17:02:43 +0530

    sign in and sign up form

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,8 +1,57 @@
-import Header from "../Header/Header.jsx"
-const Login = () => {
-  return (
-    <Header/>
-  )
-}
+import { useState } from "react";
+import Header from "../Header/Header.jsx";
 
-export default Login
+const Login = () => {
+  const [isSignInForm, setIsSignInForm] = useState(true);
+
+  const toggleSignUpForm = () => {
+    setIsSignInForm(!isSignInForm);
+  };
+
+  return (
+    <div className="relative h-screen w-screen">
+      <Header />
+      <div className="absolute inset-0">
+        <img
+          src="https://assets.nflxext.com/ffe/siteui/vlv3/fc164b4b-f085-44ee-bb7f-ec7df8539eff/d23a1608-7d90-4da1-93d6-bae2fe60a69b/IN-en-20230814-popsignuptwoweeks-perspective_alpha_website_large.jpg"
+          alt="background"
+          className="w-full h-full object-cover"
+        />
+        <div className="absolute inset-0 bg-black opacity-60" />
+      </div>
+      <form className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-full max-w-md bg-black/75 text-white p-12 rounded-md">
+        <h1 className="text-3xl font-bold mb-6">
+          {isSignInForm ? "Sign In" : "Sign Up"}
+        </h1>
+        {!isSignInForm && (
+          <input
+            type="text"
+            placeholder="Full Name"
+            className="w-full p-4 mb-4 bg-gray-800 rounded text-white placeholder-gray-400 focus:outline-none"
+          />
+        )}
+        <input
+          type="text"
+          placeholder="Email or phone number"
+          className="w-full p-4 mb-4 bg-gray-800 rounded text-white placeholder-gray-400 focus:outline-none"
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          className="w-full p-4 mb-6 bg-gray-800 rounded text-white placeholder-gray-400 focus:outline-none"
+        />
+        <button className="w-full bg-red-600 hover:bg-red-700 transition-colors py-3 rounded font-semibold">
+          {isSignInForm ? "Sign In" : "Sign Up"}
+        </button>
+        <div className="flex justify-between items-center text-sm text-gray-400 mt-4"></div>
+        <p className="py-4 cursor-pointer" onClick={toggleSignUpForm}>
+          {isSignInForm
+            ? "New to Netflix? Sign Up Now"
+            : "Already Registered? Sign In Now."}
+        </p>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
